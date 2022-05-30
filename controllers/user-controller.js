d7ef7acbce9b9c483f0242f36ca7330ed307e832
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-29 17:58:51 -0700

    write method that gets one user by id

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -7,5 +7,28 @@ const userController = {
           .select('-__v')
           .then(dbUserData => res.json(dbUserData))
           .catch(err => res.status(500).json(err));
+    },
+
+    // get single user by id
+    getUserById({ params }, res) {
+        User.findOne({ _id: params.id })
+          .populate({
+              path: 'friends',
+              select: '-__v'
+          })
+          .populate({
+              path: 'thoughts',
+              select: '-__v'
+          })
+          .select('-__v')
+          .then(dbUserData => {
+            //   if no user found, send error
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID.' }); 
+                return;
+            }
+            res.json(dbUserData);
+          })
+          .catch(err => res.status(500).json(err));
     }
 };
