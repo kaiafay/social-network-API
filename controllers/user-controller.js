56a7d7a31bcbbb58640d3db181945ce0bf48a871
Kaia Scheirman <kfscheirman18@gmail.com>
2022-05-30 20:18:17 -0700

    write method that removes a friend from a user's friend list

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -78,5 +78,18 @@ const userController = {
               res.json(dbUserData);
           })
           .catch(err => res.status(500).json(err));
+    },
+
+    // remove friend from friend list
+    removeFriend({ params }, res) {
+        User.findOneAndUpdate({ _id: params.userId }, { $pull: { friends: params.friendId }}, { new: true })
+        .then(dbUserData => {
+            if(!dbUserData) {
+                res.status(404).json({ message: 'No user found with this ID.'});
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => res.status(500).json(err));
     }
 };
