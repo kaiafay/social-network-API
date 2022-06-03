08218dbbd8692c98482aff45a4b6fa13abed4e9f
Kaia Scheirman <kfscheirman18@gmail.com>
2022-06-02 20:24:30 -0700

    write method that removes a reaction from a thought

--- controllers/thought-controller.js
+++ controllers/thought-controller.js
@@ -92,7 +92,18 @@ const thoughtController = {
         .catch(err => res.status(500).json(err));
     },
 
-    
+    // remove reaction from a thought
+    removeReaction({ params }, res) {
+        Thought.findOneAndUpdate({ _id: params.thoughtId }, { $pull: { reactions: { reactionId: params.reactionId }}}, { new: true, runValidators: true})
+        .then(dbThoughtData => {
+            if(!dbThoughtData) {
+                res.status(404).json({ message: 'No thought found with this ID.' });
+                return;
+            }
+            res.json(dbThoughtData);
+        })
+        .catch(err => res.status(500).json(err));
+    }
 };
 
 module.exports = thoughtController;
